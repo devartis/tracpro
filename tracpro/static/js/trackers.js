f3ba98ea39d895712e68de27e6309ac28899aefd
Fernando Aramendi <fernando@devartis.com>
2016-08-05 11:35:22 -0300

    added styles to format group rule formset

--- tracpro/static/js/trackers.js
+++ tracpro/static/js/trackers.js
@@ -1,6 +1,8 @@
 $(function () {
     $('.group_rule-formset').formset({
         addText: '+ add another group rule',
-        deleteText: 'X'
+        addCssClass: 'group_rule-formset-add btn btn-primary',
+        deleteText: '',
+        deleteCssClass: 'glyphicon glyphicon-trash'
     });
 });
